de6910ac4fc21d13007a80014dca745f4cdb8405
ValKart <vlebental@gmail.com>
2021-06-20 22:27:04 +0300

    Completed task 3

---
+++ src/org/appline/tasks/Task3.java
@@ -0,0 +1,32 @@
+package org.appline.tasks;
+
+
+public class Task3 {
+    public static void main(String[] args) {
+
+
+        int[] myArray = {5, 10, 15, 20, 25};
+        int first = myArray[0];
+        int last = myArray[myArray.length - 1];
+        int medium = myArray[(myArray.length - 1) / 2];
+
+        myArray[0] = last;
+        myArray[myArray.length - 1] = first;
+
+        int total = last + medium;
+        System.out.println("Результат суммы первого и среднего элемента:" + total);
+
+
+
+
+        }
+
+
+    }
+
+
+//for (int i =0; i < myArray.length; i++) {
+//System.out.println(myArray[i]);
+
+//System.out.println(medium);
+//System.out.println(last);

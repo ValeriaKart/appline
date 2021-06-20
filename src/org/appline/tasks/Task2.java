4fc5c689873510976618475b763a49af378985b9
ValKart <vlebental@gmail.com>
2021-06-20 16:59:13 +0300

    Completed task 2

--- src/org/appline/tasks/Task2.java
+++ src/org/appline/tasks/Task2.java
@@ -1,9 +1,8 @@
 package org.appline.tasks;
 
-public class Task1 {
+public class Task2 {
     public static void main(String[] args) {
-
-        System.out.println("Hello World!");
-
+        double var1 = 12345.6789;
+        byte var2 = 127;
     }
 }

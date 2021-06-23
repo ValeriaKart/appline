9394e7ba321043024f42e99080f24c77aa73874b
ValKart <vlebental@gmail.com>
2021-06-23 14:46:09 +0300

    Completed Task 5.

---
+++ src/org/appline/tasks/Task5.java
@@ -0,0 +1,36 @@
+package org.appline.tasks;
+
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Введите первое число:");
+        double a = input.nextDouble();
+
+        System.out.println("Введите второе число:");
+        double b = input.nextDouble();
+
+        System.out.println("Введите символ соответствующей арифметической операции +, -, *, /:");
+        String symbol = input.next();
+
+        switch(symbol) {
+            case "+":
+                System.out.println(a + b);
+                break;
+            case "-":
+                System.out.println(a - b);
+                break;
+            case "*":
+                System.out.println(a * b);
+                break;
+            case "/":
+                System.out.println(a /b);
+                break;
+            default:
+                System.out.println("Данная операция не поддерживается.");
+        }
+
+    }
+}

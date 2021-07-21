c35d91c32ea50015183814e905097e0926fdf0d3
ValKart <vlebental@gmail.com>
2021-07-21 16:41:23 +0300

    Changed validation in Final Task 2.

--- src/org/appline/tasks/F.java
+++ src/org/appline/tasks/F.java
@@ -2,7 +2,7 @@ package org.appline.tasks;
 
 import java.util.Scanner;
 
-public class FinalTask2 {
+public class F {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Введите, пожалуйста уравнение для решения без пробелов в данном формате:" +
@@ -11,40 +11,46 @@ public class FinalTask2 {
                 "(на лат. раскладке клавиатуры, обозначает неизвестное) в любом порядке." +
                 "\nНапример, x+5=7:");
         String str = input.nextLine();
+        System.out.println(validateInputAndCompute(str));
+    }
 
-        char a = str.charAt(0);
-        char op = str.charAt(1);
-        char b = str.charAt(2);
-        char eq = str.charAt(3);
-        char c = str.charAt(4);
+    private static boolean validateInputAndCompute(String input) {
+        char a = input.charAt(0);
+        char op = input.charAt(1);
+        char b = input.charAt(2);
+        char eq = input.charAt(3);
+        char c = input.charAt(4);
 
-        if (str.length() == 5 && eq == '=') {
+        if (input.length() == 5 && eq == '=') {
             int num1, num2;
-            if (a == 'x' && Character.isDigit(b) && Character.isDigit(c)) {
+            if ((a == 'x' && Character.isDigit(b) && Character.isDigit(c))) {
                 num1 = Character.getNumericValue(b);
                 num2 = Character.getNumericValue(c);
-                if (op == '-') {
+                if (op == '+') {
                     num1 = -num1;
                 }
-                System.out.println("x = " + (num2 - num1));
-            } else if (b == 'x' && Character.isDigit(a) && Character.isDigit(c)) {
+            } else if (Character.isDigit(a) && b == 'x' && Character.isDigit(c)) {
                 num1 = Character.getNumericValue(a);
                 num2 = Character.getNumericValue(c);
-                if (op == '-') {
+                if (op == '+') {
                     num1 = -num1;
+                } else if (op == '-') {
                     num2 = -num2;
                 }
-                System.out.println("x = " + (num2 - num1));
-            } else if (c == 'x' && Character.isDigit(a) && Character.isDigit(b)) {
+            } else if (Character.isDigit(a) && Character.isDigit(b) && c == 'x') {
                 num1 = Character.getNumericValue(a);
                 num2 = Character.getNumericValue(b);
                 if (op == '-') {
                     num2 = -num2;
                 }
-                System.out.println("x = " + (num1 + num2));
             } else {
-                System.out.println("Уравнение не по формату.");
+                return false;
             }
+            System.out.println("x = " + (num1 + num2));
+            return true;
+        } else {
+            return false;
         }
     }
+
 }
